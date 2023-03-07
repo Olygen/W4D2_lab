4e7569ad6262292fe69f6dcad717fa845e77ce40
Oligen <olga.sarukhanova@gmail.com>
2023-03-07 13:23:32 -0500

    next step1

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ function minusOne(num) {
   minusOne(10);        // prints 9
   minusOne(100);       // prints 99
   minusOne(Infinity);  // prints Infinity
-  
+
   function makeSentence(str1, str2, str3) {
     console.log(str1 + ' ' + str2 + ' ' + str3 + '.');
   }
 makeSentence('I', 'want', 'to know github');  // prints "I want github."
+
+function getLastElement(arr) {
+    console.log(arr[arr.length - 1]);
+  }
+
+getLastElement([1, 2, 3, 4, 5, 6]);       // prints 6
+getLastElement(['a', 'b', 'c']);          // prints 'c'
+getLastElement([[1, 2, 3], [4, 5, 6]]);   // prints [4, 5, 6]
