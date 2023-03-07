c96bffc43229d7f3d314bdead6efa16499b0ee88
Oligen <olga.sarukhanova@gmail.com>
2023-03-07 13:20:39 -0500

    next step

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ console.log('Practicing my git commands');
 function minusOne(num) {
     console.log(num - 1);
   }
+  minusOne(10);        // prints 9
+  minusOne(100);       // prints 99
+  minusOne(Infinity);  // prints Infinity
+  
+  function makeSentence(str1, str2, str3) {
+    console.log(str1 + ' ' + str2 + ' ' + str3 + '.');
+  }
+makeSentence('I', 'want', 'to know github');  // prints "I want github."
